e3c26c25ae1d7d9f43df18f7fbdb28608c43c74c
Vitor Martins Melo <vitormmelo04@gmail.com>
2026-09-22 16:56:19 -0300

    Enhance CandidateEntity with creation timestamp and update application properties for Hibernate configuration

--- src/main/java/com/vitor/gestao_vagas/modules/CandidateEntity.java
+++ src/main/java/com/vitor/gestao_vagas/modules/CandidateEntity.java
@@ -1,17 +1,27 @@
 package com.vitor.gestao_vagas.modules;
 
+import jakarta.annotation.Generated;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.validator.constraints.Length;
 
 import lombok.Data;
 
 @Data
+@Entity(name = "candidates")
 public class CandidateEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
     @NotBlank ()
     @Pattern (regexp = "\\S+" , message = "O campo [Username]  não pode ter espaços")
@@ -24,4 +34,6 @@ public class CandidateEntity {
     private String description;
     private String curriuculum;
 
+    @CreationTimestamp 
+    private LocalDateTime createdAt;
 }
